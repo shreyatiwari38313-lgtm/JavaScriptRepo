f541481e1c3d2d7ef2d0a824e9c3be8da7d7fdf1
shreyatiwari38313-lgtm <shreyatiwari38313@gmail.com>
2025-08-31 14:08:07 +0000

    second commit

---
+++ JSCodes/first.js
@@ -0,0 +1,19 @@
+console.log(" shreya");
+//DataTypes in js
+
+//number -for integer type values.Range - 
+//bigint - for integers that are too big that have range beyond number
+//         used for huge data (in facebook and radix like sites)
+//string - usually written in double  quotes ""
+//boolean - true/false 
+//null - standalone value ,can be assigned to another variable 
+//undefined  - written when no value is assigned to any number ,string or any kind of datatype
+//symbol - when we have to give some unique value (usually used when we use react)
+
+
+//---NON-Primitive data types
+//object - controversial data type
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
+console.log(typeof("shreya")); //string
