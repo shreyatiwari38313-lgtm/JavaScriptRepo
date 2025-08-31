a7f95b0f2533416a34a97fa3bb98c41b82146767
shreyatiwari38313-lgtm <shreyatiwari38313@gmail.com>
2025-08-31 19:36:10 +0000

    fifthcommitdone

---
+++ JSCodes/string.js
@@ -0,0 +1,17 @@
+//**************STRINGS*******************
+// const name ="shreya";
+// const repoCount = 50;
+// //concatenating strings
+// console.log(name + repoCount + " Value");
+
+//Better way to concatenate strings => using "BackTICKS"
+//BAckTicks => in backticks we make placeholders "${}" and
+//  in that user can directly inject variables
+
+//**IMP NOTE
+// console.log(`Hello System my name is ${name} and my age is ${repoCount}`);
+// **
+
+//additionally we can apply functions in variables say name.length()
+//*****String declaration */
+const newNAme = new String("shreya Tiwari");
